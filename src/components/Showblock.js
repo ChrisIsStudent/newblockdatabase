c278bee4c7593b25adf1c922cf3545eef7ecf022
被抛弃闷头写代码 <minnylin0000@gmail.com>
2018-10-17 21:08:08 +1100

    display show all blocks

--- src/components/Showblock.js
+++ src/components/Showblock.js
@@ -4,65 +4,47 @@ import {connect} from 'react-redux'
 import Box from "grommet/components/Box";
 import React from "react";
 import Blockchain from '../Blockchain/Blockchain'
+import Heading from "grommet/components/Heading";
 
 class Showblock extends Component {
     constructor(props) {
         super(props)
 
         this.state = {
-            block:[]
-
+            blockTable: []
         }
 
         this.blockchain = new Blockchain
+        this.blocks = {}
         this.blockchain.loadFromFile(response => {
-
-    this.setState(
-        {
-            block:response
-        }
-    )
-            console.log(response)
-
+            this.blocks = response
+            let arr = []
+            for(let block of this.blocks) {
+                arr.push(<tr>
+                    <td>{block.index}</td>
+                    <td>{block.hash}</td>
+                </tr>)
+            }
+            this.setState({
+                blockTable: arr
+            })
         })
     }
 
-
-
-
     render() {
         const blockArray = JSON.stringify(this.blockchain.blockchain);
-        // {
-        //     blockArray.map((block)=>{
-        //         return (
-        //             <Box  align="center">
-        //
-        //             <div>
-        //                 <table>
-        //                     <tr>
-        //                         <td>{block.indent}</td>
-        //                     </tr>
-        //                     <tr>
-        //                         <td>{block.data}</td>
-        //                     </tr>
-        //                     <tr>
-        //                         <td>{block.hash}</td>
-        //                     </tr>
-        //                 </table>
-        //             </div>
-        //             </Box>
-        //         )
-        //     })
-        // }
         return (
-            <Box  align="center">
-
-
-
-                <Box>
-                    {blockArray}
+            <Box>
+                <Heading align="center">All blocks</Heading>
+                <Box  align="center">
+                    <table>
+                        <tr>
+                            <th>index</th>
+                            <th>hash</th>
+                        </tr>
+                        {this.state.blockTable}
+                    </table>
                 </Box>
-
             </Box>
         )
     }
